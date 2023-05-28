6fd4d46e9f0dad84a37556ba2e04d1e9ce02fb1f
VanHai2404 <hovanhai24042003@gmai.com>
2023-05-28 17:34:00 +0700

    add domain classes

--- ShopJoy/src/main/java/com/edu/shop/domain/Order.java
+++ ShopJoy/src/main/java/com/edu/shop/domain/Order.java
@@ -1,5 +1,20 @@
 package com.edu.shop.domain;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+	private int orderId;
+	private Date orderDate;
+	private int customerId;
+	private double amount;
+	private short status;
+	
 
 }
