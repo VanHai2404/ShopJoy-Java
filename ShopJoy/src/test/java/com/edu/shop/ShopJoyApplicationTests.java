ff04190be453337fcea5b0323878010ae29542f0
VanHai2404 <hovanhai24042003@gmai.com>
2023-05-28 16:48:27 +0700

    git commit library

---
+++ ShopJoy/src/test/java/com/edu/shop/ShopJoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopJoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
