ec55fc9e0ae0378d1d6db9eb4a5e95de08e3ffb9
VanHai2404 <hovanhai24042003@gmai.com>
2023-06-01 01:42:57 +0700

    add product

--- ShopJoy/src/main/java/com/edu/shop/model/ProductDto.java
+++ ShopJoy/src/main/java/com/edu/shop/model/ProductDto.java
@@ -2,23 +2,26 @@ package com.edu.shop.model;
 
 import java.util.Date;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product {
+public class ProductDto {
 	private int productId;
 	private String name;
 	private int quantity;
 	private double untiPrice;
 	private String image;
+	private MultipartFile ImageFile;
 	private String description;
 	private double discount;
 	private Date enterdDate;
 	private short status;
-	private int categoryId;
+	private Long categoryId;
 	
 
 }
