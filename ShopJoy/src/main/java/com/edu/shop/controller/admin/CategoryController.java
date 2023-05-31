bce5b4b96765b8b3a432af2da9c0cb781cc0e5f4
VanHai2404 <hovanhai24042003@gmai.com>
2023-06-01 01:48:37 +0700

    update Category Controller

--- ShopJoy/src/main/java/com/edu/shop/controller/admin/CategoryController.java
+++ ShopJoy/src/main/java/com/edu/shop/controller/admin/CategoryController.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,15 +48,9 @@ public class CategoryController {
 			dto.setIsEdit(true);
 			map.addAttribute("category",dto);
 			return new ModelAndView("admin/categories/categories",map);
-		}
-		
-		
+		}		
 		map.addAttribute("message","Category is not existed !");
-		return new ModelAndView("forward:/admin/categories",map);
-		
-		
-		
-		
+		return new ModelAndView("forward:/admin/categories",map);	
 	}
 	
 	@GetMapping("delete/{categoryId}")
