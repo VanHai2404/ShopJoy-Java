ed1b88b27db01396b0e846a85a4beca57376c54f
VanHai2404 <hovanhai24042003@gmai.com>
2023-06-01 01:45:23 +0700

    update class main

--- ShopJoy/src/main/java/com/edu/shop/ShopJoyApplication.java
+++ ShopJoy/src/main/java/com/edu/shop/ShopJoyApplication.java
@@ -1,13 +1,30 @@
 package com.edu.shop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.edu.shop.config.StorageProperties;
+import com.edu.shop.service.StorageService;
+
+import groovyjarjarantlr4.v4.runtime.misc.Args;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class ShopJoyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopJoyApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return(args ->{
+			storageService.init();
+			
+		});
+		
 	}
 
 }
