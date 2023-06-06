5e12d935c96ac7d92d51a086f365d0fc9d31372e
VanHai2404 <hovanhai24042003@gmai.com>
2023-06-06 15:10:43 +0700

    update entity account

--- ShopJoy/src/main/java/com/edu/shop/domain/Account.java
+++ ShopJoy/src/main/java/com/edu/shop/domain/Account.java
@@ -1,8 +1,12 @@
 package com.edu.shop.domain;
+import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +20,21 @@ public class Account {
 	@Id
 	@Column(length = 30)
 	private String username;
+	@Column(length = 100)
+	private String fullname;
 	@Column(length = 60,nullable = false)
 	private String password;
+	@Column(length = 100)
+	private String email;
+	@Column(length = 200)
+	private String image;
+	@Temporal(TemporalType.DATE)
+	private Date birthday;
+	@Column(length = 200)
+	private String phone;
+	@Column(length = 50)
+	private String gender;
+	
 	
 	
 
--- ShopJoy/src/main/java/com/edu/shop/model/AccountDto.java
+++ ShopJoy/src/main/java/com/edu/shop/model/AccountDto.java
@@ -1,5 +1,10 @@
 package com.edu.shop.model;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
+
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +17,18 @@ public class AccountDto {
 	@NotEmpty
 	private String username;
 	@NotEmpty
+	private String fullname;
+	@NotEmpty
 	private String password;
+	@NotEmpty
+	private String email;
+	
+	private MultipartFile ImageFile;
+	private String image;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date birthday;
+
+	private String phone;
+	private String gender;
 
 }
